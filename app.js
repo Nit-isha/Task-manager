330bed8d71755267844886863ff98a5789d212d7
Nitisha <nitishagupta.1999@gmail.com>
2022-01-11 20:25:24 +0530

    feat: add front-end

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
 require("dotenv").config();
 
 //middleware: pre-built
+app.use(express.static("./public"));
 app.use(express.json()); //if we don't use then we will not have any data in req.body
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager app");
-});
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound); //for any route other than /api/v1/tasks
 
-const port = 5000;
+const port = 3000;
 
 const start = async () => {
   try {
