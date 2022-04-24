312b21df6ff5087c9f1602a00830eddcb06285cf
Nitisha <nitishagupta.1999@gmail.com>
2022-04-24 13:01:02 +0530

    heroku deploy 6

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
+const connectDB = require("./db/connect");
 
 //middleware: pre-built
 app.use(express.static("./public"));
